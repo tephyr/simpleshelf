5de8f454f159c38a0c44d5ae28e27bdf1d5308dc
Andrew Ittner <aji@rhymingpanda.com>
2011-09-22 03:45:16 +0000

    Successfully pull data from couchdb

--- simpleshelf/_attachments/code/library.js
+++ simpleshelf/_attachments/code/library.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
     // reverse colors on the welcome msg
     $('#sidebar').css({backgroundColor: 'black', color: 'white'});
+    
+    // prep vars
+    window.library = new Library();
+    
+    // load a set of books
+    library.fetch();
+    
+    // this will display zero, since the fetch is asynchronous
+    // use a backbone view!
+    $('#items').html('<b>Item count: '+library.length+'</b>');
 });
