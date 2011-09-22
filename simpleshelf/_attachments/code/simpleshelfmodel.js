ae061a7006ca7793b330ebf2f252f0fe0cc95725
Andrew Ittner <aji@rhymingpanda.com>
2011-09-22 03:44:02 +0000

    backbone models for simpleshelf

---
+++ simpleshelf/_attachments/code/simpleshelfmodel.js
@@ -0,0 +1,25 @@
+/**
+ * Model for simpleshelf
+ */
+
+var Book = Backbone.Model.extend({
+    initialize: function(){}
+});
+
+var Library = Backbone.Collection.extend({
+    model: Book,
+    url: '/simpleshelf/_design/simpleshelf/_view/all',
+    parse: function(response) {
+        var results = [];
+        if (response.rows){
+            var bookJSON;
+            for(var x=0; x < response.rows.length; x++){
+                bookJSON = response.rows[x];
+                results.push({'id': bookJSON.id, 
+                              'title': bookJSON.title});
+            }
+        }
+        
+        return results;
+    } 
+});
