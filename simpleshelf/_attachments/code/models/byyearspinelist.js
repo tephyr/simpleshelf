8c2fa813ade4fe09fa58c40307964030f51ab637
Andrew Ittner <aji@rhymingpanda.com>
2012-05-23 19:59:05 -0700

    Customize for handling years

--- simpleshelf/_attachments/code/models/byyearspinelist.js
+++ simpleshelf/_attachments/code/models/byyearspinelist.js
@@ -16,16 +16,20 @@
 	},
 
     parse: function(response){
-        var results = [], row, values;
+        var results = [], row, values, year;
         if (response.rows){
             for (var x = 0; x < response.rows.length; x++){
                 row = response.rows[x];
+                year = parseInt(row.key);
+                if (_.isNaN(year)){
+                    year = null;
+                }
                 values = {
                 	'_rev': row.value._rev,
                 	'id': row.id,
                 	'latestDateFinished': row.value.latestDateFinished,
                 	'title': row.value.title,
-                	'year': row.key
+                    'year': year
                 };
                 results.push(values);
             }
@@ -43,6 +47,17 @@
         // sort, make uniq
         allYears.sort();
         return _.uniq(allYears, true);
+    },
+
+    /**
+     * Return list of spines within given year
+     */
+    getSpinesByYear: function(year){
+        return this.filter(function(model){
+            if (model.get('year') == year) {
+                return true;
+            }
+        });
     }
 
 });
