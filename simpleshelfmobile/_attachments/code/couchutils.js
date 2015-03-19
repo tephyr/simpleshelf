9e7bcadbb2c522d394f73b762dd986f9c4a5b3c1
Andrew Ittner <aji@rhymingpanda.com>
2015-03-19 14:14:04 -0700

    Add login check, use appSetting model

--- simpleshelfmobile/_attachments/code/couchutils.js
+++ simpleshelfmobile/_attachments/code/couchutils.js
@@ -2,18 +2,44 @@
  * Utilities for accessing CouchDB.
  */
 define([
-    "jquery"
-], function($) {
+    "jquery",
+    "settings"
+], function($, appSettings) {
     var couchUtils = {
+
+        /**
+         * Determine if current session is active.
+         * @return Promise
+         **/
+        isLoggedIn: function() {
+            var dfrd = $.Deferred();
+
+            $.ajax({
+                url: appSettings.get("urlPrefix") + "/_session",
+                dataType: "json"
+            }).done(function(data) {
+                // Payload must have userCtx.name !== null.
+                if (_.has(data, "userCtx") && (!_.isNull(data.userCtx.name))) {
+                    dfrd.resolve();
+                } else {
+                    dfrd.reject();
+                }
+            }).fail(function() {
+                dfrd.reject();
+            })
+
+            return dfrd;
+        },
+
         /**
          * Login to CouchDB
          * @return Promise
          */
-        login: function(userName, password, urlPrefix) {
+        login: function(userName, password) {
             // $.Deferred callbacks: done, fail, always
             return $.ajax({
                 type: "POST",
-                url: urlPrefix + "/_session",
+                url: appSettings.get("urlPrefix") + "/_session",
                 dataType: "json",
                 data: {
                     name: userName,
@@ -22,10 +48,10 @@ define([
             });
         },
 
-        logout: function(urlPrefix) {
+        logout: function() {
             return $.ajax({
                 type: "DELETE",
-                url: urlPrefix + "/_session"
+                url: appSettings.get("urlPrefix") + "/_session"
             });
         }
 
