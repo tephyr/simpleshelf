d47db17c366d9c35999da4b024c74454e515037b
Andrew Ittner <aji@rhymingpanda.com>
2015-03-24 09:51:52 -0700

    Add module to setup and broker app events
    Provides two functions to be called after app object is ready.

---
+++ simpleshelfmobile/_attachments/code/appevents.js
@@ -0,0 +1,47 @@
+"use strict";
+// Start the main app logic.
+define([
+    "underscore",
+    "couchutils"
+], function(_, couchUtils) {
+    // app is already instantiated (as singleton), so simply add events to it.
+    var eventInitializer = {
+        setupAppEvents: function (app) {
+            app.on({
+                "app:requestLogin": function(data) {
+                    console.info("[app]", "requestLogin", data);
+                    couchUtils.login(data.username, data.password)
+                        .done(function() {
+                            console.log("Logged in!");
+                            // Proceed to main page.
+                            app.trigger("app:navigate", {url: "main"});
+                        })
+                        .fail(function() {
+                            console.warn("Login failed!");
+                            // Stay on current page.
+                            // TODO: warn user about login failure.
+                        });
+                },
+                "app:navigate": function(data) {
+                    // This call is a little different than the native Backbone navigate,
+                    // to keep it similar to all other calls: {url: String, options: Object}.
+                    console.info("[app]", "navigate", data);
+                    app.router.navigate(data.url, _.extend({}, data.options));
+                }
+
+            });
+        },
+
+        /**
+         * Direct way to connect views to app event handler.
+         * Each view must be connected to the event router **here**.
+         **/
+        hookupAppEvents: function (app) {
+            app.views.loginPageView.on("all", _.bind(function(eventName, data) {
+                app.trigger(eventName, data);
+            }, this));
+        }
+    };
+
+    return eventInitializer;
+});
