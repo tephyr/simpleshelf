60b25df02514e367498c15e9fc9e883f3b0a8b16
Andrew Ittner <aji@rhymingpanda.com>
2015-03-24 06:55:35 -0700

    Add Page View superclass
    Handles some Backbone/jQueryMobile work for all pages.

---
+++ simpleshelfmobile/_attachments/code/views/PageView.js
@@ -0,0 +1,41 @@
+/**
+ * Page view constructor
+ * Handles all jQuery Mobile setup for any sub-classed page.
+ */
+define([
+    "backbone"
+],
+function(Backbone) {
+    var PageView = Backbone.View.extend({
+        tagName: "div",
+        isInDOM: false,
+        _name: null,
+        getName: function() {
+            return this._name;
+        },
+
+        postRender: function() {
+            this.isInDOM = true;
+            return this;
+        },
+
+        render: function() {
+            if (this.isInDOM) {
+                console.warn(this.getName() + " already in DOM.");
+            } else {
+                if (this.model) {
+                    this.$el.html(this.template(this.model.attributes));
+                } else {
+                    this.$el.html(this.template());
+                }
+                // Set jQuery Mobile attributes here.
+                this.$el.attr("data-role", "page");
+                // Append to body.
+                $("body").append(this.$el);
+            }
+            return this;
+        }
+    });
+
+    return PageView;
+});
