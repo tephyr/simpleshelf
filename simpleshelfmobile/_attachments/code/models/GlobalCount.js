949b0ddeb0b47139e933b86af995cd790af2bcd1
Andrew Ittner <aji@rhymingpanda.com>
2015-03-25 17:48:12 -0700

    Customize return values: format numbers by thousands

--- simpleshelfmobile/_attachments/code/models/GlobalCount.js
+++ simpleshelfmobile/_attachments/code/models/GlobalCount.js
@@ -4,9 +4,10 @@
  **/
 define([
     "underscore",
+    "underscore.string",
     "backbone",
     "app"
-], function(_, Backbone, app) {
+], function(_, _s, Backbone, app) {
 
     var GlobalCountModel = Backbone.Model.extend({
         url: function() {
@@ -27,6 +28,15 @@ define([
 
             return results;
         },
+
+        toJSON: function() {
+            var result = this.attributes;
+            // Customize the strings.
+            _.each(_.pairs(this.attributes), function(pair) {
+                result[pair[0] + "_fmt"] = _s.numberFormat(pair[1], 0);
+            });
+            return result;
+        }
 
     });
 
