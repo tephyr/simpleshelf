c59e00f68ecc2ef4f98386f95405fcc0f1079a5b
Andrew Ittner <aji@rhymingpanda.com>
2015-03-31 13:27:54 -0700

    Improve currently reading list

--- simpleshelfmobile/_attachments/code/views/MainPageView.js
+++ simpleshelfmobile/_attachments/code/views/MainPageView.js
@@ -18,16 +18,22 @@ function(_, Backbone, Handlebars, template) {
 
         render: function() {
             // Show books in "reading" state.
-            console.info("Reading count", this.books.size());
+            var currentlyReadingCount = this.books.size();
+            console.info("Reading count", currentlyReadingCount);
 
-            this.$("#main-reading-count").text(this.books.size());
+            this.$("#main-reading-count").text(currentlyReadingCount);
+
+            if (currentlyReadingCount > 0) {
+                this.$("#main-reading").empty().append(this.books.map(function(book) {
+                    return "<li>" + book.get("title") + "</li>";
+                }));
+            } else {
+                this.$("#main-reading").empty().append("<li>Currently reading no books.</li>");
+            }
+
             this.$("#main-book-instances-fmt").text(this.model.formatSimply("_book_instances"));
             this.$("#main-author-instances-fmt").text(this.model.formatSimply("_author_instances"));
             this.$("#main-tag-instances-fmt").text(this.model.formatSimply("_tag_instances"));
-
-            this.$("#main-reading").append(this.books.map(function(book) {
-                return "<li>" + book.get("title") + "</li>";
-            }));
 
             return this;
         },
