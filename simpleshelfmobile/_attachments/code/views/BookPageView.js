6fad917aa7d6a8b747d38fd8cbad127cb8b93a28
Andrew Ittner <aji@rhymingpanda.com>
2015-04-03 11:28:24 -0700

    Add author(s) rows
    This could be simplified if multiple authors are only shown as a simple string.

--- simpleshelfmobile/_attachments/code/views/BookPageView.js
+++ simpleshelfmobile/_attachments/code/views/BookPageView.js
@@ -9,10 +9,30 @@ function(_, Backbone) {
     var BookPage = Backbone.View.extend({
         render: function() {
             // Fill out existing structure.
-            var fieldsStandardText = ["title", "isbn", "publisher", "notesPublic", "notesPrivate"];
+            var fieldsStandardText = ["title", "isbn", "publisher", "notesPublic", "notesPrivate"],
+                authors;
+
             _.each(fieldsStandardText, function(field) {
                 this.$("#book-" + field).html(this.model.get(field));
             }, this);
+
+            // Show either author or authors rows.
+            authors = this.model.get("authors");
+            if (!_.isArray(authors) || authors.length === 0) {
+                // Hide both.
+                this.$(".author, .authors").hide();
+            } else {
+                // REFACTOR: use just one line, if simply using text for multiple authors.
+                if (authors.length === 1) {
+                    this.$(".authors").hide();
+                    this.$(".author").show();
+                    this.$(".author.ui-block-b div").html(authors[0]);
+                } else {
+                    this.$(".author").hide();
+                    this.$(".authors").show();
+                    this.$(".authors.ui-block-b div").html(authors.join(", "));
+                }
+            }
 
             return this;
         }
