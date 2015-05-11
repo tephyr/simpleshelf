81c1e35f66977f56baafff29717ef6e160bbdd9b
Andrew Ittner <aji@rhymingpanda.com>
2015-05-11 15:15:26 -0700

    Track rendering count, refresh listview after 1st render

--- simpleshelfmobile/_attachments/code/views/MainPageView.js
+++ simpleshelfmobile/_attachments/code/views/MainPageView.js
@@ -13,6 +13,7 @@ function(_, Backbone, Handlebars, template) {
 
         initialize: function() {
             this.books = null;
+            this._renderedCount = 0;  // Track how many times this was rendered.
             return this;
         },
 
@@ -30,10 +31,22 @@ function(_, Backbone, Handlebars, template) {
             } else {
                 this.$("#main-reading").empty().append("<li>Currently reading no books.</li>");
             }
+            try {
+                // console.info("[MainPageView]", "# of times rendered", this._renderedCount);
+                // Don't choke on this, which only happens on the first render.
+                // Subsequent renders need this call because the list always gets emptied & refilled.
+                if (this._renderedCount > 0) {
+                    this.$("#main-reading").listview("refresh");
+                }
+            } catch(exc) {
+                console.error("[MainPageView]", "listview refresh; OK to ignore on 1st load", this._renderedCount);
+            }
 
             this.$("#main-book-instances-fmt").text(this.model.formatSimply("_book_instances"));
             this.$("#main-author-instances-fmt").text(this.model.formatSimply("_author_instances"));
             this.$("#main-tag-instances-fmt").text(this.model.formatSimply("_tag_instances"));
+
+            this._renderedCount++;
 
             return this;
         },
