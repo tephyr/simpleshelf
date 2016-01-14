b48431aeee67aee573c9ae546d5eb9a677c55871
Andrew Ittner <aji@rhymingpanda.com>
2016-01-13 16:55:53 -0800

    Convert main.js to module format

--- app/code/main.js
+++ app/code/main.js
@@ -1,85 +1,26 @@
-// Sets the require.js configuration for this application.
-require.config( {
+var appsetup = require("./appsetup.js"),
+    appevents = require("./appevents.js");
 
-    baseUrl: "code",
+console.info(window.pageLoadedAt);
 
-    // 3rd party script alias names
-    paths: {
+// Load plugins and widgets on the global $.
+// require(["widgets/tags"]);
 
-        // Core Libraries
-        "jquery": "lib/jquery",
-        "jquerymobile": "lib/jquery.mobile-1.4.5",
-        "underscore": "lib/underscore",
-        "underscore.string": "lib/underscore.string",
-        "backbone": "lib/backbone",
-        "handlebars": "lib/handlebars",
-        "text": "lib/text"
-    },
+// Override BB sync.
+appsetup.overrideBackboneSync(Backbone, _);
 
-    // Sets the configuration for third party scripts that are not AMD compatible
-    shim: {
+this.app = App; // singleton
 
-        // "backbone": {
-        //     "deps": [ "underscore", "jquery" ],
-        //     "exports": "Backbone"
-        // }
+// Setup top-level app objects.
+app.router = new Router();
+appevents.setupAppEvents(app);
+appevents.hookupAppEvents(app);
 
-    }
+// Tidy initial view.
+$("body").removeClass("splash");
 
-});
+// Start Backbone routing.
+Backbone.history.start();
 
-// Includes File Dependencies
-require([
-    "jquery",
-    "backbone",
-    "underscore",
-    "app",
-    "router",
-    "appevents",
-    "appsetup"
-], function ( $, Backbone, _, App, Router ) {
-
-    $( document ).on( "mobileinit",
-
-        // Set up the "mobileinit" handler before requiring jQuery Mobile's module
-        function () {
-
-            // Prevents all anchor click handling including the addition of active button state and alternate link bluring.
-            $.mobile.linkBindingEnabled = false;
-
-            // Disabling this will prevent jQuery Mobile from handling hash changes
-            $.mobile.hashListeningEnabled = false;
-
-            $.mobile.defaultPageTransition = "slide";
-        }
-    );
-
-    require( [ "jquerymobile" ], function () {
-
-        console.info(window.pageLoadedAt);
-
-        // Load plugins and widgets on the global $.
-        require(["widgets/tags"]);
-
-        // Override BB sync.
-        require("appsetup").overrideBackboneSync(Backbone, _);
-
-        this.app = App; // singleton
-
-        // Setup top-level app objects.
-        app.router = new Router();
-        require("appevents").setupAppEvents(app);
-        require("appevents").hookupAppEvents(app);
-        // require("appevents").setupJQMEvents(app);
-
-        // Tidy initial view.
-        $("body").removeClass("splash");
-
-        // Start Backbone routing.
-        Backbone.history.start();
-
-        // Start app.
-        this.app.run();
-
-    });
-});
+// Start app.
+this.app.run();
