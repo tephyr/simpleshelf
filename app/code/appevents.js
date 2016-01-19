d8f74c4a4eb3968f60bca830b91bf65bb30d533e
Andrew Ittner <aji@rhymingpanda.com>
2016-01-19 11:13:03 -0800

    Add titles to console statements

--- app/code/appevents.js
+++ app/code/appevents.js
@@ -5,23 +5,25 @@ var _ = require("underscore"),
 // app is already instantiated (as singleton), so simply add events to it.
 module.exports = {
     setupAppEvents: function (app) {
+        var _logHeader = "[app]";
+
         app.on({
             "app:bookChanged": function(data) {
                 // Notify catalog that metadata must be refreshed.
-                console.info("[app]", "bookChanged", data);
+                console.info(_logHeader, "bookChanged", data);
                 app.catalog.metadataUpToDate = false;
             },
 
             "app:requestLogin": function(data) {
-                console.info("[app]", "requestLogin", data);
+                console.info(_logHeader, "requestLogin", data);
                 couchUtils.login(data.username, data.password)
                     .done(function() {
-                        console.log("Logged in!");
+                        console.log(_logHeader, "Logged in!");
                         // Proceed to main page.
                         app.trigger("app:navigate", {url: "main"});
                     })
                     .fail(function() {
-                        console.warn("Login failed!");
+                        console.warn(_logHeader, "Login failed!");
                         // Stay on current page.
                         // TODO: warn user about login failure.
                     });
@@ -31,7 +33,7 @@ module.exports = {
                 var url;
                 // This call is a little different than the native Backbone navigate,
                 // to keep it similar to all other calls: {url: String, options: Object}.
-                console.info("[app]", "navigate", data);
+                console.info(_logHeader, "navigate", data);
                 // By default, trigger the route method; to disable, set ``trigger==false``.
                 // This is opposite of Backbone's default.
                 if (_.has(data, "url")) {
