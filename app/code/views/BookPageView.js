d60f63e3c18b1a5e32d5314c75a4f1820c643ba7
Andrew Ittner <aji@rhymingpanda.com>
2016-01-23 10:30:18 -0800

    Convert status object to list for template

--- app/code/views/BookPageView.js
+++ app/code/views/BookPageView.js
@@ -18,8 +18,14 @@ var BookPageView = Backbone.View.extend({
 
     render: function() {
         var data = this.model.toJSON();
+
         // Add hints for template.
         data._multipleAuthors = (this.model.get("authors").length > 1)
+        // Convert status object into list of objects with identical key/value keys.
+        data._statusList = _.map(_.pairs(this.model.get("status")), function(pair) {
+            return {statusKey: pair[0], statusValue: pair[1]};
+        });
+
         this.$el.html(this._template(data));
         return this;
     },
