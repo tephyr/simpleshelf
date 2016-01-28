1b6021d312a54f0a149a820fa19f3943ce68edc3
Andrew Ittner <aji@rhymingpanda.com>
2016-01-28 15:37:42 -0800

    Ensure that all Handlebars helper tags have the right data.

--- app/code/views/BookPageView.js
+++ app/code/views/BookPageView.js
@@ -15,15 +15,22 @@ var BookPageView = Backbone.View.extend({
         return this;
     },
 
+    /**
+     * Render a book model.
+     * Ensures that all data needed by the template exists (empty arrays instead of undefined).
+     * @return {Object} BookPageView
+     */
     render: function() {
         var data = this.model.toJSON();
 
         // Add hints for template.
         data._multipleAuthors = (this.model.get("authors").length > 1)
+        data._activities = data.activities || [];
         // Convert status object into list of objects with identical key/value keys.
-        data._statusList = _.map(_.pairs(this.model.get("status")), function(pair) {
+        data._statusList = _.map(_.pairs(data.status || []), function(pair) {
             return {statusKey: pair[0], statusValue: pair[1]};
         });
+        data._tags = data.tags || [];
 
         this.$el.html(this._template(data));
         return this;
