18f8261175d9d6057c1466685c29fa30d1ca989c
Andrew Ittner <aji@rhymingpanda.com>
2016-05-06 09:35:46 -0700

    Add testing instructions

--- tasks/test-phantom.js
+++ tasks/test-phantom.js
@@ -2,6 +2,9 @@ module.exports = function(gulp, settings) {
     var path = require('path'),
         mochaPhantomJS = require('gulp-mocha-phantomjs');
      
+    /**
+     * Run tests in console using PhantomJS.
+     */
     gulp.task('test-phantom', ['build-tests'], function () {
         return gulp
             .src(path.join(settings.testOutputPath, 'runner.html'))
