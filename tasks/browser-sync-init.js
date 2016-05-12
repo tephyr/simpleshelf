4ffc4e3768e5f2044d3a3d5b6051c542fcde6765
Andrew Ittner <aji@rhymingpanda.com>
2016-05-12 09:36:25 -0700

    Build tests before initializing BrowserSync

--- tasks/browser-sync-init.js
+++ tasks/browser-sync-init.js
@@ -5,11 +5,12 @@ module.exports = function(gulp, settings) {
     /**
      * Initialize the browser sync server (static).
      */
-    gulp.task('browser-sync-init', function() {
+    gulp.task('browser-sync-init', ['build-tests'], function(cb) {
         settings.dynamic.browserSync.init({
             server: {
                 baseDir: settings.testOutputPath
             }
         });
+        cb();
     });
 };
