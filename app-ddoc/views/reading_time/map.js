c5495ebd6362d3fcd21b75db806677c61f6fad22
Andrew Ittner <aji@rhymingpanda.com>
2016-05-13 13:11:42 -0700

    CouchDB view of days spent reading each book

---
+++ app-ddoc/views/reading_time/map.js
@@ -0,0 +1,41 @@
+/**
+ * Show reading time for books that have a start & finish.
+ */
+function(doc){
+    var _ = require('views/lib/underscore'),
+        moment = require('views/lib/moment'),
+        sortedActivities,
+        start = null,
+        hasTerm = false,
+        result = 0;
+
+    // Find books.
+    if (doc.type && doc.type === "book") {
+        // Find books with activities.
+        if (doc.activities && doc.activities.length > 0) {
+            // Get sorted activities.
+            sortedActivities = _.sortBy(doc.activities, function(activity) {
+                return activity.date;
+            });
+
+            // Find pairs of started/finished activities.
+            _.each(sortedActivities, function(activity) {
+                if (activity.action && activity.action === "book.read.started") {
+                    start = moment(activity.date);
+                } else if (activity.action && activity.action === "book.read.finished") {
+                    if (!_.isNull(start)) {
+                        hasTerm = true;
+                        result += moment(activity.date).diff(start, 'days');
+
+                        // Reset
+                        start = null;
+                    }
+                }
+            });
+
+            if (hasTerm) {
+                emit(null, result);
+            }
+        }
+    }
+}
