75a51f7cef4bfcbe1fa01c942d3c60aeb022419f
Andrew Ittner <aji@rhymingpanda.com>
2016-09-04 10:09:20 -0700

    Modify navbar when on home page

--- app/code/views/NavbarView.js
+++ app/code/views/NavbarView.js
@@ -2,6 +2,7 @@
  * Navigation bar view.
  */
 var Backbone = require("backbone"),
+    _ = require('underscore'),
     Handlebars = require("handlebars"),
     NavbarTemplate = require("./templates/navbar.html");
 
@@ -18,6 +19,20 @@ var navigationView = Backbone.View.extend({
     render: function() {
         this.$el.html(this._template());
         return this;
+    },
+
+    /** CUSTOM FUNCTIONS **/
+
+    /**
+     * Modify this view when on home page.
+     * @param {Object} data {home: bool}
+     */
+    setHomeView: function(data) {
+        if (_.has(data, 'home') && data.home) {
+            this.$el.removeClass('home');
+        } else {
+            this.$el.addClass('home');
+        }
     },
 
     /** EVENTS **/
