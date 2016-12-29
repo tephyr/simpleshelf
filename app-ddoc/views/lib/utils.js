13ced6c5a0f34ebdb6b18c86abced07bb030fac7
Andrew Ittner <aji@rhymingpanda.com>
2016-12-29 15:29:15 -0800

    Parens are not alphabetic characters

--- app-ddoc/views/lib/utils.js
+++ app-ddoc/views/lib/utils.js
@@ -3,7 +3,7 @@
  * Courtesy http://stackoverflow.com/a/2450689/562978
  **/
 function isLetter(s) {
-    return s.match("^[a-zA-Z\(\)]+$");    
+    return s.match("^[a-zA-Z]+$");
 }
 
 // Use CommonJS format, split into objects based on basic type of utility.
