5ccfb33fd4633cf3b5e82adde261d3617298d4cd
Andrew Ittner <aji@rhymingpanda.com>
2017-10-08 13:15:18 -0700

    Add default import module, convert TagInputView to imports

--- app/code/views/TagInputView.js
+++ app/code/views/TagInputView.js
@@ -3,14 +3,12 @@
  * Tabbing issue on Firefox: https://github.com/selectize/selectize.js/issues/396; fix has languished
  * as PR since 2015-02-20.
  **/
-var $ = require("jquery"),
-    _ = require("underscore"),
-    Backbone = require("backbone"),
-    Handlebars = require("handlebars"),
-    Selectize = require("selectize"),
-    TagInputTemplate = require("./templates/taginput.html");
+import {$, _, Backbone} from 'DefaultImports';
+import Handlebars from "handlebars";
+import Selectize from "selectize";
+import TagInputTemplate from "./templates/taginput.html";
 
-var TagInputView = Backbone.View.extend({
+const TagInputView = Backbone.View.extend({
     _logHeader: "[TagInputView]",
     tagName: "div",
 
