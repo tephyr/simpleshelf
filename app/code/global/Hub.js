b27ab883fa86d612da64b1d203a1bf66aafbd8d1
Andrew Ittner <aji@rhymingpanda.com>
2017-11-29 17:33:55 -0800

    Move app:requestlogin event to Hub

--- app/code/global/Hub.js
+++ app/code/global/Hub.js
@@ -1,10 +1,12 @@
 import {Backbone, _} from 'DefaultImports';
+import {CouchUtils} from 'couchutils';
 
 /**
  * Hub module for applicaiton.
  * @type {Object}
  * Brokered events:
  * - app:navigate           (any view signals a route change)
+ * - app:requestlogin
  * - router:navigate        (Hub triggers a route change)
  * - routechanged           (router navigated to different route)
  */
@@ -15,6 +17,7 @@ class HubModule {
 
     setupEvents() {
         this.on('app:navigate', this.onNavigate);
+        this.on('app:requestlogin', this.onRequestLogin);
     }
 
     onNavigate(data) {
@@ -39,6 +42,21 @@ class HubModule {
         // By default, trigger the route method; to disable, set ``trigger==false``.
         // This is opposite of Backbone's default.
         this.trigger('router:navigate', url, _.extend({trigger: true}, data.options));
+    }
+
+    onRequestLogin(data) {
+        console.info(this._logHeader, "requestlogin", data);
+        CouchUtils.login(data.username, data.password)
+            .done(() => {
+                console.log(this._logHeader, "Logged in!");
+                // Proceed to main page.
+                this.trigger("app:navigate", {url: "main"});
+            })
+            .fail(() => {
+                console.warn(this._logHeader, "Login failed!");
+                // Stay on current page.
+                // TODO: warn user about login failure.
+            });
     }
 };
 
