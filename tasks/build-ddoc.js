25a03c52c0162daf5cb0a09fca0e1e6d6769ad25
Andrew Ittner <aji@rhymingpanda.com>
2018-05-05 14:42:17 -0700

    Compile design doc locally

--- tasks/build-ddoc.js
+++ tasks/build-ddoc.js
@@ -1,13 +1,18 @@
 import path from 'path';
+import fs from 'fs';
 import del from 'del';
+import compile from 'couchdb-compile';
 
 module.exports = function(gulp, settings) {
+    const outputTemp = `${settings.ddocOutput}-temp`;
+
     /**
      * Clean the output directory.
      */
     gulp.task('_clean:ddoc', function() {
         return del([
-            settings.ddocOutput
+            settings.ddocOutput,
+            outputTemp
         ]);
     });
 
@@ -16,20 +21,32 @@ module.exports = function(gulp, settings) {
      **/
     gulp.task('_copy_ddoc', ['_clean:ddoc'], () => {
         return gulp.src(settings.globs.ddoc)
-            .pipe(gulp.dest(settings.ddocOutput));
+            .pipe(gulp.dest(outputTemp));
     });
 
     /**
      * Take global modules and install them to {ddocOutput/views/lib}.
      */
     gulp.task('_copy_ddoc_modules', ['_copy_ddoc'], () => {
-        const moduleDest = path.join(settings.ddocOutput, 'views', 'lib');
+        const moduleDest = path.join(outputTemp, 'views', 'lib');
         return gulp.src(settings.ddocModules)
             .pipe(gulp.dest(moduleDest));
     });
 
     /**
+     * Compile design doc directory to single JSON file.
+     */
+    gulp.task('_compile-ddoc', ['_copy_ddoc_modules'], (cb) => {
+        compile(outputTemp, (err, doc) => {
+            fs.mkdirSync(settings.ddocOutput);
+            fs.writeFileSync(path.join(settings.ddocOutput, 'designdoc.json'), JSON.stringify(doc));
+            del.sync([outputTemp]);
+            cb();
+        });
+    });
+
+    /**
      * Run all sub-tasks in order.
      */
-    gulp.task('build-ddoc', ['_copy_ddoc_modules']);
+    gulp.task('build-ddoc', ['_compile-ddoc']);
 };
