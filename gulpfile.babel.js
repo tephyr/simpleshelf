5d4015de44545b57b01626ca9fa56de26eaa8b30
Andrew Ittner <aji@rhymingpanda.com>
2019-05-13 16:46:09 -0700

    Enable testInBrowser task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -68,10 +68,10 @@ import { lint } from './tasks/lint';
 import {uiTest} from "./tasks/ui-test.js";
 import {browserSyncInit} from "./tasks/browser-sync-init.js";
 import {browserSyncReload} from "./tasks/browser-sync-reload.js";
-import * as testInBrowser from "./tasks/test-in-browser";
+import {testInBrowser} from "./tasks/test-in-browser";
 import {buildForDocker} from "./tasks/build-for-docker.js";
 
-export { buildApp, buildDDoc, buildDocs, buildForDocker, bundleLib, code, lint, push, uiTest };
+export { buildApp, buildDDoc, buildDocs, buildForDocker, bundleLib, code, lint, push, testInBrowser };
 
 /**
  * Show settings for this task runner.
