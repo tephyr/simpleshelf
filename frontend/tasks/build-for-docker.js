5e0e99c825fe426b044e13107ce100762120bc8f
Andrew Ittner <aji@rhymingpanda.com>
2022-03-02 16:05:07 -0800

    Remove unused gulp task

--- frontend/tasks/build-for-docker.js
+++
@@ -1,9 +0,0 @@
-import {series} from 'gulp';
-import {buildApp} from './build-app';
-import {copyDocs} from './copy-docs';
-import {buildDDoc} from './build-ddoc';
-
-/**
- * build-for-docker: prep everything in ./output related to the node server.
- */
-export const buildForDocker = series(buildApp, copyDocs, buildDDoc);
